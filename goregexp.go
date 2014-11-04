fb3fd80844fc3a93ee6370900b109b21f2096867
VonC <vonc@laposte.net>
2014-11-04 22:39:08 +0100

    Basic methods for accessing regexp results

--- goregexp.go
+++ goregexp.go
@@ -27,3 +27,88 @@ func NewReres(s string, r *regexp.Regexp) *Reres {
 	matches := r.FindAllStringSubmatchIndex(s, -1)
 	return &Reres{r, s, matches, 0, 0}
 }
+
+/* full initial text on which the regex was applied */
+func (rr *Reres) Text() string {
+	return rr.s
+}
+
+/* Check if there is any match */
+func (rr *Reres) HasAnyMatch() bool {
+	return len(rr.matches) > 0
+}
+
+/* Check if there is one more match */
+func (rr *Reres) HasNext() bool {
+	return rr.i < len(rr.matches)
+}
+
+/* Refers to the next match, for Group() to works with */
+func (rr *Reres) Next() {
+	rr.previous = rr.matches[rr.i][1]
+	rr.i = rr.i + 1
+}
+
+/* Get back to the first match */
+func (rr *Reres) ResetNext() {
+	rr.i = 0
+	rr.previous = 0
+}
+
+/* String from the last match to current one */
+func (rr *Reres) Prefix() string {
+	mi := rr.matches[rr.i]
+	return rr.s[rr.previous:mi[0]]
+}
+
+/* String from current match to the end ofthe all string */
+func (rr *Reres) Suffix() string {
+	mi := rr.matches[rr.i]
+	res := ""
+	if len(rr.s) > mi[1] {
+		res = rr.s[mi[1]:]
+	}
+	return res
+}
+
+/* First character of the current match */
+func (rr *Reres) FirstChar() uint8 {
+	mi := rr.matches[rr.i]
+	return rr.s[mi[0]]
+}
+
+/* Test if first character of the current match is an escape */
+func (rr *Reres) IsEscaped() bool {
+	mi := rr.matches[rr.i]
+	return rr.s[mi[0]] == '\\'
+}
+
+/* Full string matched for the current group */
+func (rr *Reres) FullMatch() string {
+	mi := rr.matches[rr.i]
+	return rr.s[mi[0]:mi[1]]
+}
+
+/* Check if the ith group if present in the current match */
+func (rr *Reres) HasGroup(j int) bool {
+	res := false
+	mi := rr.matches[rr.i]
+	if len(mi) > (j*2)+1 {
+		if mi[j*2] > -1 {
+			if mi[j*2] < mi[(j*2)+1] {
+				res = true
+			}
+		}
+	}
+	return res
+}
+
+/* return the ith group string, if present in the current match */
+func (rr *Reres) Group(i int) string {
+	res := ""
+	if rr.HasGroup(i) {
+		mi := rr.matches[rr.i]
+		res = rr.s[mi[i*2]:mi[(i*2)+1]]
+	}
+	return res
+}
